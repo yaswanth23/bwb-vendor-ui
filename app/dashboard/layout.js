20c340e22bd7a46d19169f0bed194c0cacbf03e4
yash <yashwanth.k@mooo.farm>
2023-12-04 10:21:11 +0530

    basic setup

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,18 +1,18 @@
-// import { getServerSession } from "next-auth";
-// import { redirect } from "next/navigation";
-// import DashboardLayout from "../ui/dashboard/dashboardLayout/dashboardLayout";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import DashboardLayout from "../ui/dashboard/dashboardLayout/dashboardLayout";
 
-// const Layout = async ({ children }) => {
-//   const session = await getServerSession();
-//   if (!session) {
-//     redirect("/login");
-//   }
+const Layout = async ({ children }) => {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/login");
+  }
 
-//   return (
-//     <>
-//       <DashboardLayout>{children}</DashboardLayout>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <DashboardLayout>{children}</DashboardLayout>
+    </>
+  );
+};
 
-// export default Layout;
+export default Layout;
